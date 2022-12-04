8ff42db60bf9d08592a95676c8b17647bd8b9f95
sevikpetrov <petrovmal@gmail.com>
2022-12-04 20:10:47 +0000

    end of basket

--- API/Startup.cs
+++ API/Startup.cs
@@ -61,7 +61,11 @@ namespace API
             app.UseRouting();
 
             app.UseCors(opt => {
-                opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
+                opt.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .SetIsOriginAllowed(origin => true) 
+                .WithOrigins("http://localhost:3000");
             });
 
             app.UseAuthorization();
